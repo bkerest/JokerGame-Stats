eb80925c20892921fc4267ef5f494070d08650f3
gkiop <gkiop@LAPTOP-MSJ24JNG>
2022-02-20 17:25:56 +0200

    Libraries

---
+++ JokerGameStats/src/jokergamestats/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package jokergamestats.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
