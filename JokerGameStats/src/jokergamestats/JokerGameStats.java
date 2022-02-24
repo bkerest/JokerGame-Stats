c73cd440e463c0f121c4647beab78ed73b2702e7
gkiop <gkiop@LAPTOP-MSJ24JNG>
2022-02-24 18:12:40 +0200

    Merge origin/main into main

--- JokerGameStats/src/jokergamestats/JokerGameStats.java
+++ JokerGameStats/src/jokergamestats/JokerGameStats.java
@@ -5,12 +5,20 @@
  */
 
 package jokergamestats;
+import java.util.List;
+import javax.persistence.*;
 
 /**
  *
  * @author vker
  */
 public class JokerGameStats {
+    static EntityManagerFactory emf;
+    static EntityManager em;
+    private static void createEMFandEM() {
+        emf = Persistence.createEntityManagerFactory(JokerGameStatsPU);
+        em = emf.createEntityManager();
+    }
 
     /**
      * @param args the command line arguments
